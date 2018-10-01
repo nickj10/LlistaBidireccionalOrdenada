94d59a9eebac7727748cc4985597492177c6d7dc
nickj10 <jas.jimenez411@gmail.com>
2018-10-02 00:16:26 +0200

    feat: Create template for each function in module
    Add the structs needed for the linked lists and the prototypes of the
    functions in the module Llista. Create the templates for each one of
    the functions in the .c file.

--- llista.c
+++ llista.c
@@ -1,3 +1,57 @@
 #include "llista.h"
 
+Llista LLISTA_crea() {
+	Llista l;
+
+	return l;
+}
+
+void LLISTA_insertDavant (Llista *l, int e) {
+	
+}
+
+void LLISTA_insertDarrere (Llista *l, int e) {
+
+}
+
+int LLISTA_consulta (Llista l) {
+
+}
+
+void LLISTA_elimina (Llista *l) {
+
+}
+
+void LLISTA_avanca (Llista *l) {
+
+}
+
+void LLISTA_retrocedeix (Llista *l) {
+
+}
+
+void LLISTA_vesInici (Llista *l) {
+
+}
+
+void LLISTA_vesFinal (Llista *l) {
+
+}
+
+int LLISTA_inici (Llista l) {
+
+}
+
+int LLISTA_final (Llista l) {
+	
+}
+
+int LLISTA_buida (Llista l) {
+
+}
+
+void LLISTA_destrueix (Llista *l) {
+
+}
+
 
