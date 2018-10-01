b0a2b7adf27d5e85b21e8a7a752595c9b8829379
nickj10 <jas.jimenez411@gmail.com>
2018-10-02 00:24:34 +0200

    test: Create the first test template in main.c
    Write a simple code that asks the user to enter an integer after
    creating a list of integers. Then, it adds the integer to the list.
    Finally, it will destroy the list created previously.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
+	// Used for testing the Llista module
+	Llista numeros;
+	int num = 0;
+
+	numeros = LLISTA_crea();
+	
+	printf ("Enter an integer: ");
+	scanf ("%d", &num);
+
+	// Insert the number entered by user to the list of numbers
+	LLISTA_insertaDelante (&numeros, num);
+
+
+	// Destroy the list
+	LLISTA_destrueix (&numeros);
 
 	return 0;
 }
