30a884f657294a8749e5514084409319c883521c
nickj10 <jas.jimenez411@gmail.com>
2018-10-04 08:28:55 +0200

    test: Add new  tests for the functions of the module
    The program asks for a number which represents the total number of integers to input. Then it will add the numbers to the list and show them on the screen.

--- main.c
+++ main.c
@@ -4,17 +4,28 @@
 int main() {
 	// Used for testing the Llista module
 	Llista numeros;
-	int num = 0;
+	int num = 0, read = 0;
+	int i = 0, count = 0;
 
 	numeros = LLISTA_crea();
 	
-	while (LLISTA_final(numeros)) {
+	printf ("How many numbers do you want to enter? ");
+	scanf ("%d", &count);
+		
+	while (i < count) {
 		printf ("Enter an integer: ");
 		scanf ("%d", &num);
 
 		// Insert the number entered by user to the list of numbers
 		LLISTA_insertDavant (&numeros, num);
 
+		LLISTA_vesInici (&numeros);
+
+		while (!LLISTA_final(numeros)) {
+			read = LLISTA_consulta(numeros);
+			printf ("Num %d: %d\n", i+1, read);
+			LLISTA_avanca(&numeros);
+		}
 	}
 	// Destroy the list
 	LLISTA_destrueix (&numeros);
