eb19bf0275c9006470519b90cd451efce4e393f6
Cole <31732303+nickj10@users.noreply.github.com>
2018-10-07 20:50:22 +0200

    doc: Add the document header comments to llista.c

--- llista.c
+++ llista.c
@@ -1,3 +1,13 @@
+/*********************************************************
+ *
+ * @Purpose: This file contains the implementation of the
+ *	functions for the Sorted Bidirectional Linked List.
+ * @Autor: Nicole Marie Jimenez Burayag
+ * @Data creacio: 01/10/2018
+ * @Data ultima modificacio: 07/10/2018
+ * 
+ ********************************************************/
+
 #include "llista.h"
 
 /*******************
